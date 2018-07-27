aad5f38ad51e45ff54b49461413385da8cf04b5f
DESKTOP-0G656D9\likeit_student <cottoncandy3a@gmail.com>
2018-07-27 13:27:55 +0900

    PTra01

--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -21,23 +21,25 @@ public class PTra01_07 {
 
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
-
-
-
-
-
-
-
+		bo = true;
+		by = 3;
+		s = 64;
+		c = 'て';
+		i = 14035;
+		l = 1234567890L;
+		f = 11.4F;
+		d = 8.5;
 
 
 		// ★ それぞれの変数の中身を出力してください
-
-
-
-
-
-
-
+		System.out.println(bo);
+		System.out.println(by);
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
 
 
 	}
